e60bc969d349c26060771af7601ccf0f49158d6b
chrimill <chris@hexial.com>
2017-04-10 16:49:23 -0600

    Revert "Updated user interface"
    This reverts commit 259f800207ddf5981d688b871f3265de8d94e33b.

--- public/main.js
+++ public/main.js
@@ -1,84 +1,35 @@
 // client side JS
-// Skeleton whiteboard code from socket.io
 'use strict';
 
 (function() {
-  var url = window.location.hostname;
-  var socket = io.connect();
+
+  var socket = io();
   var canvas = document.getElementsByClassName('whiteboard')[0];
   var colors = document.getElementsByClassName('color');
   var context = canvas.getContext('2d');
 
-  // Get navigation bar elements
-  var open = document.getElementById('openLink');
-  var save = document.getElementById('saveLink');
-  var clear = document.getElementById('clearLink');
-
-  canvas.width = document.body.clientWidth;
-  canvas.height = document.body.clientHeight;
-
-  // Store the a temporary canvas
-  var canvasHx = document.createElement('canvas');
-  var contextHx = canvasHx.getContext('2d');
-
   var current = {
     color: 'black'
   };
-
-  // flag for drawing
   var drawing = false;
 
-  // Manage mouse events
   canvas.addEventListener('mousedown', onMouseDown, false);
   canvas.addEventListener('mouseup', onMouseUp, false);
   canvas.addEventListener('mouseout', onMouseUp, false);
   canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
 
-  // Manage navigation clicks
-  open.addEventListener('click', openImage, false);
-  save.addEventListener('click', redraw, false);
-  clear.addEventListener('click', clearCanvas, false);
-
-  // Manage touch events
-  // From: https://developer.mozilla.org/en/docs/Web/API/Touch_events
-  canvas.addEventListener('touchstart', onTouch, false);
-  canvas.addEventListener('touchend', onTouch, false);
-  canvas.addEventListener('touchcancel', onTouch, false);
-  canvas.addEventListener('touchmove', onTouch, false);
-
   for (var i = 0; i < colors.length; i++){
     colors[i].addEventListener('click', onColorUpdate, false);
   }
 
-  // Process instructions from server
-  socket.on('clear canvas', clearCanvas);
   socket.on('drawing', onDrawingEvent);
+
   socket.on('image', loadImage);
+
 
   window.addEventListener('resize', onResize, false);
   onResize();
 
-  // TO DO - touch event handler
-  function onTouch(e){
-    var touches = event.changedTouches,
-    first = touches[0];
-  }
-
-  function sendDrawingToServer(data){
-    var img = new Image();
-    img.src = canvas.toDataURL(); // returns a PNG
-    console.log('img' + img);
-    console.log('img src ' + img.src);
-    // socket.emit('send-canvas', img);
-    console.log('Drawing sent!');
-  }
-
-  // TO DO - open image functionality
-  function openImage(){
-    console.log("Image will open");
-  }
-
-  // Loads an image from server
   function loadImage(data){
     // encode into base64
     var uint8Arr = new Uint8Array(data.buffer);
@@ -87,16 +38,13 @@
         binary += String.fromCharCode(uint8Arr[i]);
     }
     var base64String = window.btoa(binary);
-    // console.log(base64String);
     var img = new Image();
     // load image
     img.onload = function () {
       context.drawImage(img, 0, 0);
     };
-    
     img.src = 'data:image/jpeg;base64,' + base64String;
-
-    console.log('Image loaded');
+    console.log('Got the image');
   }
 
   function drawLine(x0, y0, x1, y1, color, emit){
@@ -163,59 +111,10 @@
     drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
   }
 
-  function redraw() {
-    console.log("redrawing");
-    var imgData = context.getImageData(0,0, canvas.width, canvas.height);
-    context.putImageData(imgData, 0, 0);
-
-    /*
-    canvasHx.width = canvas.width;
-    canvasHx.height = canvas.height;
-    contextHx.drawImage(canvas, 0, 0);
-    canvas.width = 1000;
-    context.drawImage(canvasHx, 0, 0);
-    */
-  }
-
-  /**
-   TO DO 
-  */
-  function saveCanvas(){
-    console.log('Canvas saved!');
-  }
-
-  // clear canvas
-  function clearCanvas() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    socket.emit('clear canvas');
-  }
-
   // make the canvas fill its parent
   function onResize() {
-    canvasHx.width = canvas.width;
-    canvasHx.height = canvas.height;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    contextHx.drawImage(canvas, 0, 0);
-    context.drawImage(canvasHx, 0, 0);
-    redraw();
-    console.log("Canvas resized");
-    // context = canvas.getContext('2d');
-    // context.onload = function () {
-    //   context.drawImage(canvasHx, 0, 0, canvasHx.width, canvasHx.height, 0, 0, canvas.width, canvas.height);
-    // };
-
-    /*
-    console.log("u resized");
-    // set canvas dimensions
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    // Save canvas as image data
-    var imgData = context.getImageData(0, 0, canvas.width, canvas.height);
-    // Copy back
-    context.putImageData(imgData, 0, 0);
-    // redraw();
-    */
   }
 
 })();
